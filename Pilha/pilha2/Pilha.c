9f44712a540650b47e81fdcea4c02845714ec8a6
Gabriel Vieira Sbardelotti <gvsbardelotti@gmai.com>
2023-04-03 20:05:24 -0300

    Melhorando algumas coisas

--- Pilha/pilha2/Pilha.c
+++ Pilha/pilha2/Pilha.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Pilha.h"
+#include <string.h>
 
 struct no{
-    char n;
+    char letra;
     struct no *prox;
 };
 
 typedef struct no No;
+typedef No* Pilha;
 
-Pilha* CriaPilha(){
-    Pilha* p = (Pilha *) malloc(sizeof(Pilha));
-    if(p != NULL) *p = NULL;
-    return p;
+Pilha CriaPilha(){
+    return NULL;
 }
 
-void LiberaPilha(Pilha* p){
-    if(p != NULL){
-        No *no;
-        while((*p) != NULL){
-            no = *p;
-            *p = (*p)->prox;
-            free(no);
-        }
-        free(p);
+void LiberaPilha(Pilha p){
+    No *no;
+    while(p != NULL){
+        no = p;
+        p = p->prox;
+        free(no);
     }
 }
 
-int TamanhoPilha(Pilha* p){
+int TamanhoPilha(Pilha p){
     if(p == NULL) return 0;
     int count = 0;
-    No *no = *p;
+    No *no = p;
     while(no != NULL){
         count++;
         no = no->prox;
@@ -38,86 +34,40 @@ int TamanhoPilha(Pilha* p){
     return count;
 }
 
-int PilhaVazia(Pilha* p){
+int PilhaVazia(Pilha p){
     if(p == NULL) return 1;
-    if((*p) == NULL) return 1;
     return 0;
 }
 
-int InserePilha(Pilha* p, char n){
-    if(p == NULL) return 0;
+int InserePilha(Pilha *p, char letra){
     No *no = (No *) malloc(sizeof(No));
     if(no == NULL) return 0;
-    no->n = n;
+    no->letra = letra;
     no->prox = (*p);
     *p = no;
     return 1;
 }
 
-int RemovePilha(Pilha* p){
-    if(p == NULL) return 0;
-    if((*p) == NULL) return 0;
+void Desempilhar(Pilha *p, char *letra){
+    *letra = (*p)->letra;
     No *no = (*p);
     *p = no->prox;
     free(no);
-    return 1;
-}
-
-int ConsultaPilha(Pilha* p, char *x){
-    if(p == NULL) return 0;
-    if((*p) == NULL) return 0;
-    *x = (*p)->n;
-    return 1;
-}
-
-char Desempilhar(Pilha* p){
-    if(p == NULL) return -1;
-    if((*p) == NULL) return -1;
-    char n = (*p)->n;
-    No *no = (*p);
-    *p = no->prox;
-    free(no);
-    return n;
 }
 
 void ImprimirInvertido(char texto[]){
-    if(texto[0] == '\0') return;
-    char t = ' ';
-    int i = 0, espaco = 0;
-    while(t != '.'){
-        t = texto[i];
-        if(t == ' '){
-            espaco++;
+    Pilha p = CriaPilha();
+    int tamanho = strlen(texto);
+    for (int i = 0; i < tamanho; i++) {
+        if (texto[i] == ' ' || texto[i] == '.') {
+            while (!PilhaVazia(p)) {
+                char letra;
+                Desempilhar(&p, &letra);
+                printf("%c", letra);
+            }
+            if (texto[i] == ' ') printf(" ");
+        } else {
+            InserePilha(&p, texto[i]);
         }
-        i++;
-    }
-    espaco++;
-    Pilha *aux[espaco];
-    aux[0] = CriaPilha();
-    t = texto[0];
-    i = espaco = 0;
-    while(t != '.'){
-        if(t == ' '){
-            espaco++;
-            aux[espaco] = CriaPilha();
-            i++;
-        }else{
-            InserePilha(aux[espaco], t);
-            i++;
-        }
-        t = texto[i];
-    }
-
-    for(i = 0; i <= espaco; i++){
-        while((*aux[i])!= NULL){
-            printf("%c", Desempilhar(aux[i]));
-        }
-        printf(" ");
     }
 }
-
-
-
-
-
-
